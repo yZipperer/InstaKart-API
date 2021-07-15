65d787fbcefc94f2980c69eaad90602b5fd6dcad
unknown <yz768737@gmail.com>
2021-07-14 19:32:46 -0600

    Deletion
    I have deleted some development console.logs()

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -3,10 +3,8 @@ const User = require('../models/user');
 exports.cUser = async (req, res) => {
     const {name, email, picture} = req.user;
     const user = await User.findOneAndUpdate({email: email}, {name: name, picture: picture}, {new: true});
-    console.log("in");
     if(user){
         res.json(user);
-        console.log("updated user", user);
     } else {
         const newUser = await User({
             name: name,
@@ -14,6 +12,5 @@ exports.cUser = async (req, res) => {
             picture: picture
         }).save();
         res.json(newUser);
-        console.log("created user", newUser);
     }
 };
