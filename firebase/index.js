edfe77374887d5e4971ce2dfffa4a546ba444480
unknown <yz768737@gmail.com>
2021-07-23 20:39:36 -0600

    Minor Change

--- firebase/index.js
+++ firebase/index.js
@@ -1,7 +1,6 @@
+const admin = require('firebase-admin');
 
-var admin = require('firebase-admin');
-
-var serviceAccount = require('../config/fSAF.json');
+const serviceAccount = require('../config/fSAF.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
