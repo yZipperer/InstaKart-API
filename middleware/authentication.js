ff668e633ebfacc2a9b76771d0f89df97b5ffae6
unknown <yz768737@gmail.com>
2021-07-24 22:34:56 -0600

    Added admin Check
    Now, there is a route for checking the admin and making sure they have the required fields.

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,4 +1,5 @@
 const admin = require('../firebase');
+const User = require('../models/user');
 
 exports.authenticationCheck = async (req, res, next) => {
     try{
@@ -10,5 +11,19 @@ exports.authenticationCheck = async (req, res, next) => {
         res.status(401).json({
             err: "Invalid or expired token"
         });
+    }
+};
+
+exports.adminCheck = async (req, res, next) => {
+    const {email} = req.user;
+    const admin = await User.findOne({email: email})
+    .exec()
+
+    if(admin.role !== "admin" || admin.role === "user"){
+        res.status(403).json({
+            err: "Access Denied"
+        });
+    } else{
+        next();
     }
 };
--- routes/authentication.js
+++ routes/authentication.js
@@ -3,9 +3,10 @@ const {cUser, currentUser} = require('../controllers/authentication');
 
 const router = express.Router();
 
-const {authenticationCheck} = require('../middleware/authentication');
+const {authenticationCheck, adminCheck} = require('../middleware/authentication');
 
 router.post("/cUser", authenticationCheck, cUser); //create user
-router.post("/currentUser", authenticationCheck, currentUser); 
+router.post("/currentUser", authenticationCheck, currentUser); //gets current user
+router.post("/currentAdmin", authenticationCheck, adminCheck, currentUser); 
 
 module.exports = router;
