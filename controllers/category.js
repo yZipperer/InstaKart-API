c15cb45d5e34086011cdbd4624fa92793dd606d5
unknown <yz768737@gmail.com>
2021-08-10 13:27:23 -0600

    createCategory Method
    Created the createCategory method. You only need to specify a name and it will create a category for that name.

--- controllers/category.js
+++ controllers/category.js
@@ -1,21 +1,28 @@
 const Category = require('../models/category');
+const slugify = require('slugify');
 
-exports.createCategory = (req, res) => {
+exports.createCategory = async (req, res) => {
+    try{
+        const {name} = req.body;
+        const category = await new Category({name, slug: slugify(name)}).save();
+        res.json(category);
+    } catch (err) {
+        res.status(400).send("Category Creation Failed")
+    }
+};
+
+exports.readCategory = async (req, res) => {
 
 };
 
-exports.readCategory = (req, res) => {
+exports.updateCategory = async (req, res) => {
 
 };
 
-exports.updateCategory = (req, res) => {
+exports.removeCategory = async (req, res) => {
 
 };
 
-exports.removeCategory = (req, res) => {
-
-};
-
-exports.listCategories = (req, res) => {
+exports.listCategories = async (req, res) => {
 
 };
